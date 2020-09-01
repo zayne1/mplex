f367e8d5697a6f0399d6e7bd12a25e13550c84b2
Zayne Arnold <Zayne.a@gmail.com>
2020-09-01 17:17:28 +0200

    CSS, JS, imgs and vids added

---
+++ images/signin.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+    <script type="text/javascript" src="jquery.js"></script>
+<title>Metroplex Streaming</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="bootstrap-responsive.min.css" /> -->
+    <link rel="stylesheet" type="text/css" href="font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="metroplex.css" />
+    
+    
+
+    <style type="text/css">
+        .menu-top
+        {
+            display: none;
+        }
+        .main-content
+        {
+            text-align: center;
+            padding-top: 30px;
+            width: 400px;
+            margin: 0px auto;
+        }
+        .sign-input
+        {
+            display: block;
+            margin: 0px auto;
+            width: 90%;
+        }
+        .sign-submit
+        {
+            display: block;
+            float: right;
+            margin-right: 10%;
+            margin-top: 15px;
+            font-size: 21px;
+            padding: 7px;
+        }
+
+    </style>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+ 
+</head>
+<body>
+    <div class="header">
+        <hr/>
+        <a href="index.php">
+            <img class="home-icon" src="icon-home.png">
+        </a>
+        <img class="logo-txt" src="logo-txt.png">
+    </div>
+    <div class="menu-top">
+        <div class="menu-top-container">
+            <div class="menu-btn watch"></div>
+            <div class="menu-btn downloads"></div>
+            <div class="menu-btn fav"></div>
+            <div class="menu-btn information"></div>
+            
+        </div>
+    </div>
+    <div class="page-name">
+        Sign in
+    </div>
+    <div class="main-content">
+        
+        <h3>Recital 2017 Wednesday</h3>
+
+        <h3>Please sign in with your password</h3>
+
+        <input type="input" name="" class="sign-input">
+        <input type="submit" name="" value="Next" class="sign-submit">
+
+        
+        
+    </div>
+
+    <footer>
+        Streaming App
+    </footer>
+
+    <script type="text/javascript" src="bootstrap.min.js"></script>
+</body>
+</html>  
+    
---
+++ images/watch.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+    <script type="text/javascript" src="jquery.js"></script>
+<title>Metroplex Streaming</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="bootstrap-responsive.min.css" /> -->
+    <link rel="stylesheet" type="text/css" href="font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="metroplex.css" />
+    
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+ 
+</head>
+<body>
+    <div class="header">
+        <hr/>
+        <a href="index.php">
+            <img class="home-icon" src="icon-home.png">
+        </a>
+        <img class="logo-txt" src="logo-txt.png">
+    </div>
+    <div class="menu-top">
+        <div class="menu-top-container">
+            <div class="menu-btn watch"></div>
+            <div class="menu-btn downloads"></div>
+            <div class="menu-btn fav"></div>
+            <div class="menu-btn information"></div>
+            
+        </div>
+    </div>
+    <div class="page-name">
+        Watch Video
+    </div>
+    <div class="main-content">
+        
+    </div>
+
+    <footer>
+        Streaming App
+    </footer>
+
+    <script type="text/javascript" src="bootstrap.min.js"></script>
+</body>
+</html>  
+    
