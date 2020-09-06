1125dae50ba23c41d85baf5a2687b9407f384db1
Zayne Arnold <Zayne.a@gmail.com>
2020-09-06 05:43:36 +0200

    small fav pg updates

--- protected/views/site/favourites.php
+++ protected/views/site/favourites.php
@@ -10,22 +10,24 @@
   <?php 
   foreach ($vidList as $vid) { 
   ?>
-      <div class="vid-countainer" style="width: 340px;height: 280px;float: left;">
+      <div class="vid-container" style="width: 340px;height: 280px;float: left;">
         <video onload="video.currentTime=sprungzeit" width="320" height="240" src="<?php echo Yii::app()->request->baseUrl .'/vid/'. $vid->file; ?>" Xautobuffer Xautoplay controls class="myVideo" style="float: left;margin-right: 10px;">
           <div class="video-fallback">
            <br>Sie benoetigen einen Browser, der HTML5 unterstuetzt.
           </div>
         </video>
+        <div class="vid-info-footer">
 
-        <?php if ($vid->fav==1) {
-        ?>
-          <a href="<?php echo Yii::app()->request->baseUrl .'/favourites?remFav='. $vid->_id; ?>" style="display: block;float: right;clear: both;text-decoration: none;">
-            <i class="icon-star pull-left icon-3x muted icon-green"></i>
-          </a>
+          <?php if ($vid->fav==1) {
+          ?>
+            <a href="<?php echo Yii::app()->request->baseUrl .'/favourites?remFav='. $vid->_id; ?>" style="display: block;float: right;clear: both;text-decoration: none;">
+              <i class="icon-star pull-left icon-3x muted icon-green"></i>
+            </a>
 
-        <?php
-        }
-        ?>
+          <?php
+          }
+          ?>
+        </div>
         
       </div>
   
