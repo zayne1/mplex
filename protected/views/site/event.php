9cd4c56bceaa842a92e79dc154f59838ddc03e35
Zayne Arnold <Zayne.a@gmail.com>
2020-09-06 02:16:21 +0200

    Events: Events view now pulling from db & using session vs url param

--- protected/views/site/event.php
+++ protected/views/site/event.php
@@ -3,43 +3,19 @@
 </div>
 <div class="main-content">
     <h3><?php echo $introSubText; ?></h3>
-
-
-<?php if($u==1) { ?>
+<?php 
+$count=1;
+foreach ($EventList as $event) {
+?>
 	<div class="event-container">
-	    <a href="#myModal1" data-toggle="modal">
-	        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/event1.png" class="event animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;">
-	        Recital 2017 Wednesday
+	    <a href="<?php echo '#myModal'.$count;?>" data-toggle="modal">
+	        <img src="<?php echo Yii::app()->request->baseUrl .'/images/'. $event->logo; ?>" class="event animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;">
+	        <?php echo $event->name; ?>
 	    </a>
 	</div>
-<?php } ?>
-<?php if($u==2) { ?>
-	<div class="event-container">
-	    <a href="#myModal2" data-toggle="modal">
-	        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/event2.jpg" class="event animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;">
-	        Recital 2017 Wednesday
-	    </a>
-	</div>
-<?php } ?>
-	<!-- <div class="event-container">
-	    <a href="#myModal" data-toggle="modal">
-	        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/event.png" class="event">
-	        Recital 2017 Thursday
-	    </a>
-	</div>
-	<div class="event-container">
-	    <a href="#myModal" data-toggle="modal">
-	        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/event.png" class="event">
-	        Recital 2017 Friday
-	    </a>
-	</div>
- -->
-
-
-
 
 	<!-- Modal -->
-	<div id="myModal1" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
+	<div id="<?php echo 'myModal'.$count; ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
 	    <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 	        <h3 id="myModalLabel">Sign in</h3>
@@ -47,44 +23,30 @@
 	    <div class="modal-body">
 	        <h3>Recital 2017 Wednesday</h3>
 
-	        <h3>Please sign in with your password</h3>
+	        <h3>Please sign in with your password: <?php echo $event->pass;?></h3>
 
-	        <?php $form=$this->beginWidget('CActiveForm', array(
-				'id'=>'login-form',
-				'enableClientValidation'=>true,
-				'clientOptions'=>array(
-					'validateOnSubmit'=>true,
-				),
-			)); ?>
-
-	        <input name="LoginForm[username]" id="LoginForm_username" type="hidden" value="admin">
-Password is "admin"
-	        <!-- <input type="input" name="" class="sign-input"> -->
-	        <?php echo $form->labelEx($model,'password'); ?>
-			<?php echo $form->passwordField($model,'password'); ?>
-			<?php echo $form->error($model,'password'); ?>
-
-	        <div>
-				<?php echo $form->checkBox($model,'rememberMe'); ?>
-				<?php echo $form->label($model,'rememberMe'); ?>
-				<?php echo $form->error($model,'rememberMe'); ?>
-			</div>
-	        
-	        <!-- <input type="submit" name="" value="Next" class="sign-submit"> -->
+	        <form id="login-form" action="/event?eventId=<?php echo $event->_id; ?>" method="post">
+		        <label for="LoginForm_password" class="required">Password <span class="required">*</span></label>					
+		        <input name="LoginForm[password]" id="LoginForm_password" type="password">			
+		        <div class="errorMessage" id="LoginForm_password_em_" style="display:none"></div>
+		        <br>
+				<input type="submit" name="" value="Next" class="sign-submit">
+        	</form>
 	        <br/>
-			<?php echo CHtml::submitButton('Next', array('class' => 'sign-submit')); ?>
-        	
-        	<?php $this->endWidget(); ?>
-
 	    </div>
 	    <div class="modal-footer">
-	    <!-- <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button> -->
-	    <!-- <button class="btn btn-primary">Save changes</button> -->
 	    </div>
 	</div>
+<?php
+$count++;
+}
+?>
+
+
+	
 
 	<!-- Modal -->
-	<div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
+	<div id="myModal0" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
 	    <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 	        <h3 id="myModalLabel">Sign in</h3>
