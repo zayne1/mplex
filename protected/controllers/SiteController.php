060eb2b84d8b2928dccae6a910ab2853357b4a1d
Zayne Arnold <Zayne.a@gmail.com>
2020-09-06 02:08:25 +0200

    SiteController updates: * Organizations: pulling from db * Events: pulling form db * Favourites: pulling form db * Favourites: Add/remove working * Multi Zip: demo working * Using sessions ( Yii::app()->request->getState ) instead of url params ( Yii::app()->request->getParam ) as they were getting too clunky

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -32,9 +32,13 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
+
+		$OrganizationList = Organization::model()->getAllOrgs();
+
 		$this->render('index', array(
                 'introText' => 'Welcome',
                 'introSubText' => 'Please select your organization',
+                'OrgList' => $OrganizationList
             )
         );
 	}
@@ -116,9 +120,10 @@ class SiteController extends Controller
 
 	public function actionEvent()
 	{
+		// $u=Yii::app()->request->getParam('u', null);
+		$orgId=Yii::app()->request->getParam('org', null);
 
 		$model=new LoginForm;
-		$u=Yii::app()->request->getParam('u', null);
 
 		// if it is ajax validation request
 		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
@@ -128,62 +133,154 @@ class SiteController extends Controller
 		}
 
 		// collect user input data
-		if(isset($_POST['LoginForm']))
+		/*if(isset($_POST['LoginForm']))
 		{
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
 				// $this->redirect(Yii::app()->user->returnUrl);
 				$this->redirect(Yii::app()->baseUrl . '/video?u='.$u);
+		}*/
+
+		if(isset($_POST['LoginForm']))
+		{
+			$eventId = Yii::app()->request->getParam('eventId', null);
+
+			// print_r(Event::model()->validatePass($_POST['LoginForm']['password'], $eventId));die;
+
+			if (Event::model()->validatePass($_POST['LoginForm']['password'], $eventId)) {
+				Yii::app()->user->setState('userEvent', $eventId);
+				$this->redirect(Yii::app()->baseUrl . '/video');
+			}
+			else
+				$this->redirect(Yii::app()->baseUrl . '/');
 		}
+
 		
 		// display the login form
 		//$this->render('login',array('model'=>$model));
+
+		$EventList = Event::model()->getEventsForOrg($orgId);
+		// print_r($EventList);die;
+		// ->test2[0]->property1;
+
 
 		$this->render('event', array(
                 'introText' => 'Events',
                 'introSubText' => 'Please select your show',
                 'model'=>$model,
-                'u'=>$u,
+                'EventList'=>$EventList,                
             )
         );
 	}
 
 	public function actionVideo()
 	{
+		$eventId = Yii::app()->user->getState('userEvent');
+		$newFavVidId = Yii::app()->request->getParam('addFav', null);
+		$newRemVidId = Yii::app()->request->getParam('remFav', null);
+
+		if(isset($newFavVidId)) {
+			if (Video::model()->addFav($newFavVidId)){
+				$this->redirect(Yii::app()->baseUrl . '/video');
+			}
+		} elseif (isset($newRemVidId)) {
+			if (Video::model()->remFav($newRemVidId)){
+				$this->redirect(Yii::app()->baseUrl . '/video');
+			}
+		}
+
+		$vidList = Video::model()->getVidsForEvent($eventId);
+		// print_r($vidList);die;
+
 		$this->render('video', array(
                 'introText' => 'Watch Video',
                 'introSubText' => '',
-                'username' => Yii::app()->user->username,
+                'vidList' => $vidList,
             )
         );
 	}
 
 	public function actionDownloads()
 	{
+		$eventId = Yii::app()->user->getState('userEvent');
+
 		$this->render('downloads', array(
                 'introText' => 'Download',
                 'introSubText' => 'Download videos to share',
-                'username' => Yii::app()->user->username,
+                // 'username' => Yii::app()->user->username,
             )
         );
 	}
 
 	public function actionFavourites()
 	{
+		$newRemVidId = Yii::app()->request->getParam('remFav', null);
+		$eventId = Yii::app()->user->getState('userEvent');
+
+		$vidList = Video::model()->getFavVidsForEvent($eventId);
+
+		if (isset($newRemVidId)) {
+			if (Video::model()->remFav($newRemVidId)){
+				$this->redirect(Yii::app()->baseUrl . '/favourites');
+			}
+		}
+
 		$this->render('favourites', array(
                 'introText' => 'Favourites',
                 'introSubText' => '',
-                'username' => Yii::app()->user->username,
+                'vidList' => $vidList
             )
         );
 	}
 
 	public function actionContactfaq()
 	{
+		$eventId = Yii::app()->user->getState('userEvent');
+
 		$this->render('contactfaq', array(
                 'introText' => 'Contact/Faq',
                 'introSubText' => '',
+            )
+        );
+	}
+
+	// remove after demo
+	public function actionMultidownloadtest()
+	{
+
+		$eventId = Yii::app()->user->getState('userEvent');
+
+		//remove after demo
+		$favVidList = Video::model()->getFavVidsForEvent($eventId);
+
+		
+
+		$zipname = 'enter_any_name_for_the_zipped_file.zip';
+		$zip = new ZipArchive;
+
+		if ($zip->open($zipname, ZipArchive::CREATE) === TRUE) {
+			// $counter = 0;
+			foreach ($favVidList as $vid) {
+			  $zip->addFile(getcwd() .'/vid/'. $vid->file, $vid->file);
+			  // $zip->setCompressionIndex($counter, ZipArchive::CM_STORE);
+			  // $counter++;
+			}
+			// print_r($zip);
+			$zip->close();
+
+			$size=filesize($zipname);
+
+		} else {
+			echo 'failed to open zip file';
+		}
+
+		// die('gg');
+		
+		$this->renderPartial('multidownloaddemoview', array(
+                'zipname' => $zipname,
+                'size' => $size
+                
             )
         );
 	}
