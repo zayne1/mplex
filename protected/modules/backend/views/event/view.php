44636f40c536017a0d7a2d7d7e47f7b0bc368ecd
Zayne Arnold <Zayne.a@gmail.com>
2020-09-11 18:59:54 +0200

    Backend: Event pg: We Can now del videos

--- protected/modules/backend/views/event/view.php
+++ protected/modules/backend/views/event/view.php
@@ -53,7 +53,24 @@
     foreach ($vidList as $vid) {
     ?>
         <div class="row vid-row-item">
-            <i class="icon-trash icon-2x"></i>
+
+            
+            <a id="<?php echo $vid->_id; ?>" href="#">
+                <i class="icon-trash icon-2x"></i>
+                <!-- We use the js below to use a controller's delete via POST as we cannot just create a param (it only accepts POST for delete.
+                We copied this code from what Yii's Gii CRUD generated to do deletions (with slight mods to return to current page:) 
+                -->
+                <script type="text/javascript">
+                    /*<![CDATA[*/
+                    jQuery(function($) {
+                    jQuery('body').on('click','#<?php echo $vid->_id;?>',function(){
+                        if(confirm('Are you sure you want to delete this item?')) 
+                        {jQuery.yii.submitForm(this,'/backend/video/delete/id/<?php echo $vid->_id;?>',{'returnUrl':window.location.href});return false;} else return false;});
+                    });
+                    /*]]>*/
+                </script>
+            </a>
+
             <img src="<?php echo Yii::app()->request->baseUrl .'/images/videothumb.png' ?>" class="video-thumb">
 
             <a href="<?php echo Yii::app()->createUrl('backend/event/view/id/' .$vid->_id); ?>" class="animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;width: 147px;">
