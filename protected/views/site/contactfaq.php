38ad8c3b739d231c343007d223159dd148fd1f47
Zayne Arnold <Zayne.a@gmail.com>
2020-09-12 22:20:26 +0200

    Front-end: Faq pg

--- protected/views/site/contactfaq.php
+++ protected/views/site/contactfaq.php
@@ -9,6 +9,36 @@
 <div class="main-content">
     <h3><?php echo $introSubText; ?></h3>
 
+    <b>Q - Can the videos be viewed by the entire internet?</b><br>
+    A - No. We designed the app to be private and secure. Only those with the Organization’s unique password can access video  les.
+    <br />
+    <br />
+    <b>Q - Do I need to Download the videos
+    to watch?</b><br>
+    A - No, but downloading lets you watch without an internet connection as well as giving you a backup copy to keep.
+    <br />
+    <br />
+    <b>Q - How Long will my Files be accessible?</b><br>
+    A - The  les are guaranteed for one year after the show. Please download the  les for lifetime access.
+    <br />
+    <br />
+    <b>Q - Can I take the  les out of the app?</b><br>
+    A - Yes. We want you to take possession of the  les and make backup copies.
+    <br />
+    <br />
+    <b>Q - Can I share the  les on Social Media?</b>
+    A - Once you download the  les you can share them across devices and social media.
+    <br />
+    <br />
+    <b>Q - Where are the  les stored on my device?</b><br> 
+    iPhone - Go to the Files app, and look for iCloud or Downloads.
+    Andriod - Go to the Files app, and look for Downloads.
+    Windows PC/MAC - go to the Downloads folder.
+    <br />
+    <br />
+    <b>Q - Do you still offer DVD or Blu-ray?</b><br>
+    A - Yes. Contact your studio or organization. They will have the discs available.
+
 
 
 
