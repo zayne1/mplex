abcc8a2cae1d331bf4df9667e9796d18a7b2c191
Zayne Arnold <Zayne.a@gmail.com>
2020-10-13 08:59:12 +0200

    Backend: Cards now have grey backgrounds

--- protected/modules/backend/views/organization/_org_event_view.php
+++ protected/modules/backend/views/organization/_org_event_view.php
@@ -1,4 +1,4 @@
-<a href="<?php echo Yii::app()->createUrl('backend/event/view/id/' . CHtml::encode($data->_id)); ?>" class="animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;width: 147px;">
+<a href="<?php echo Yii::app()->createUrl('backend/event/view/id/' . CHtml::encode($data->_id)); ?>" class="animate__animated animate__slideInLeft animate__fast" style="display:block;float: left;margin-right: 10px;width: 147px;background: #454447;">
     <img src="<?php echo Yii::app()->request->baseUrl .'/images/'. CHtml::encode($data->logo); ?>" class="event">
     <div style="background: #c21238;padding: 5px;color: #fff;text-align: center;height: 45px;">
         <?php echo CHtml::encode($data->name); ?>
