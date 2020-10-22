9a897fdfa0d9e7588b666bce948b450d893392c3
Zayne Arnold <Zayne.a@gmail.com>
2020-10-22 21:02:04 +0200

    Frontend: Added Fav Icon, updated faw font size

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -8,6 +8,7 @@ if ( isset(Yii::app()->controller->module->is_backend) && (Yii::app()->controlle
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <link rel="shortcut icon" type="image/jpg" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
